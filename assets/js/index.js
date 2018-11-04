10442739878d17bbba5af93a430c56eca2e81c69
mikepreston17 <michael.n.preston@gmail.com>
2018-11-03 20:45:33 -0500

    init

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+const addTrain = (train) => {
+
+    const {
+        name,
+        destination,
+        firstTime,
+        frequency,
+        // minutesAway
+    }
+}
+//name, destination, first time (military), frequency (min), minutes away.
+
+const calculateNextArrivalTime = (train) => {
+
+}
+
+// should be relative to current time
+
+
+// Bonus
+// update times every minute.
---
+++ assets/js/utils.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyDhjGCfJWi-8L1EtnNA1-tTGIz3R5_iG6Y",
+    authDomain: "loco-moment.firebaseapp.com",
+    databaseURL: "https://loco-moment.firebaseio.com",
+    projectId: "loco-moment",
+    storageBucket: "",
+    messagingSenderId: "538694652392"
+};
+
+firebase.initializeApp(config);
+
+var db = firebase.database();
